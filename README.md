3ebc8c00839aa672c4dd1bcdbf2410eda388b7db
kanosamasi <lhb.88@foxmail.com>
2022-07-02 16:42:28 +0800

    HomeworkSystem 数据库框架完善01

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # 电子作业系统的设计和开发
 
-[数据库设计模型](https://dbdiagram.io/d/62beb8df69be0b672c7e89f2)
+### 使用技术栈
 
-+ 数据库设计工具: dbdiagram
+1. **Spring Cloud Alibaba** 微服务框架
+    1. Nacos 服务注册和配置管理
+    2. Sentinel 流量控制
+2. **Spring Cloud** 微服务框架
+    1. Gateway 后端网关
+    2. OpenFeign 微服务通信
+3. **Spring Boot** 服务框架
+4. **Redis** 缓存
+5. **ElasticSearch** 搜索
+6. **RabbitMQ** 消息队列 异步通信
+
+### 设计模型
+
+[数据库设计模型](https://dbdiagram.io/d/62beb8df69be0b672c7e89f2)<br>
+系统框架<br>
+![系统框架](image/HomeworkSystem.png)
+
++ 数据库设计工具: [dbdiagram](https://dbdiagram.io/home)
++ 系统架构设计工具: [diagrams.net](https://github.com/jgraph/drawio-desktop)
++ 细分
+    1. User
+    2. Homework
+    3. Course
+    4. Creator
+    5. Respondent
+    6. CourseManager
+    7. Teacher
+    8. Student
+    9. Admin
+    10. Register
--- SQL/HomeworkSystem.sql
+++ SQL/HomeworkSystem.sql
@@ -1,137 +1,185 @@
-CREATE TABLE `user` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT "Anonymous",
-  `password_hash` varchar(255) NOT NULL,
-  `head_image` varchar(255),
-  `introduction` varchar(255) DEFAULT "无",
-  `email` varchar(255) DEFAULT "无",
-  `create_time` timestamp NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS `user` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `name` varchar(255) DEFAULT "Anonymous",
+        `password_hash` varchar(255) NOT NULL,
+        `head_image` varchar(255),
+        `introduction` varchar(255) DEFAULT "无",
+        `email` varchar(255) DEFAULT "无",
+        `create_time` timestamp NOT NULL
+    );
 
-CREATE TABLE `role` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(10)
-);
+CREATE TABLE
+    IF NOT EXISTS `role` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `name` varchar(10)
+    );
 
-CREATE TABLE `user_role` (
-  `uid` int,
-  `rid` int
-);
+CREATE TABLE IF NOT EXISTS `user_role` ( `uid` int, `rid` int );
 
-CREATE TABLE `result` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `isFile` boolean DEFAULT false,
-  `score` int DEFAULT 0,
-  `comment` varchar(255) DEFAULT "无",
-  `create_time` timestamp NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS `result` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `content` varchar(255) NOT NULL,
+        `isFile` boolean DEFAULT false,
+        `score` int DEFAULT 0,
+        `comment` varchar(255) DEFAULT "无",
+        `create_time` timestamp NOT NULL
+    );
 
-CREATE TABLE `student_result` (
-  `sid` int,
-  `rid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `student_result` (`sid` int, `rid` int);
 
-CREATE TABLE `question` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `extension` varchar(255),
-  `score` int DEFAULT 10,
-  `isFile` boolean DEFAULT false,
-  `answer` varchar(255) NOT NULL,
-  `comment` varchar(255) DEFAULT "无",
-  `create_time` timestamp NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS `question` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `title` varchar(255) NOT NULL,
+        `extension` varchar(255),
+        `score` int DEFAULT 10,
+        `isFile` boolean DEFAULT false,
+        `answer` varchar(255) NOT NULL,
+        `comment` varchar(255) DEFAULT "无",
+        `create_time` timestamp NOT NULL
+    );
 
-CREATE TABLE `questionType` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `typeName` varchar(255)
-);
+CREATE TABLE
+    IF NOT EXISTS `questionType` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `typeName` varchar(255)
+    );
 
-CREATE TABLE `questio_type` (
-  `qid` int,
-  `qtid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `question_type` (`qid` int, `qtid` int);
 
-CREATE TABLE `teacher_question` (
-  `tid` int,
-  `qid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `teacher_question` (`tid` int, `qid` int);
 
-CREATE TABLE `question_result` (
-  `qid` int,
-  `rid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `question_result` (`qid` int, `rid` int);
 
-CREATE TABLE `course` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `student_num` int DEFAULT 0,
-  `create_time` timestamp NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS `course` (
+        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `name` varchar(255) NOT NULL,
+        `student_num` int DEFAULT 0,
+        `create_time` timestamp NOT NULL
+    );
 
-CREATE TABLE `teacher_course` (
-  `tid` int,
-  `cid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `teacher_course` (`tid` int, `cid` int);
 
-CREATE TABLE `student_course` (
-  `sid` int,
-  `cid` int
-);
+CREATE TABLE
+    IF NOT EXISTS `student_course` (`sid` int, `cid` int);
 
-CREATE INDEX `user_role_index_0` ON `user_role` (`uid`, `rid`);
+CREATE TABLE
+    IF NOT EXISTS `question_course` (`qid` int, `cid` int);
 
-CREATE INDEX `user_role_index_1` ON `user_role` (`rid`, `uid`);
+CREATE TABLE IF NOT EXISTS `result_course` ( `rid` int, `cid` int );
 
-CREATE INDEX `student_result_index_2` ON `student_result` (`sid`, `rid`);
+CREATE UNIQUE INDEX `user_role_index_0` ON `user_role` (`uid`, `rid`);
 
-CREATE INDEX `student_result_index_3` ON `student_result` (`rid`, `sid`);
+CREATE UNIQUE INDEX `user_role_index_1` ON `user_role` (`rid`, `uid`);
 
-CREATE INDEX `questio_type_index_4` ON `questio_type` (`qid`, `qtid`);
+CREATE UNIQUE INDEX `student_result_index_2` ON `student_result` (`sid`, `rid`);
 
-CREATE INDEX `questio_type_index_5` ON `questio_type` (`qtid`, `qid`);
+CREATE UNIQUE INDEX `student_result_index_3` ON `student_result` (`rid`, `sid`);
 
-CREATE INDEX `teacher_question_index_6` ON `teacher_question` (`tid`, `qid`);
+CREATE UNIQUE INDEX `question_type_index_4` ON `question_type` (`qid`, `qtid`);
 
-CREATE INDEX `teacher_question_index_7` ON `teacher_question` (`qid`, `tid`);
+CREATE UNIQUE INDEX `question_type_index_5` ON `question_type` (`qtid`, `qid`);
 
-CREATE INDEX `question_result_index_8` ON `question_result` (`qid`, `rid`);
+CREATE UNIQUE INDEX `teacher_question_index_6` ON `teacher_question` (`tid`, `qid`);
 
-CREATE INDEX `question_result_index_9` ON `question_result` (`rid`, `qid`);
+CREATE UNIQUE INDEX `teacher_question_index_7` ON `teacher_question` (`qid`, `tid`);
 
-CREATE INDEX `teacher_course_index_10` ON `teacher_course` (`tid`, `cid`);
+CREATE UNIQUE INDEX `question_result_index_8` ON `question_result` (`qid`, `rid`);
 
-CREATE INDEX `teacher_course_index_11` ON `teacher_course` (`cid`, `tid`);
+CREATE UNIQUE INDEX `question_result_index_9` ON `question_result` (`rid`, `qid`);
 
-CREATE INDEX `student_course_index_12` ON `student_course` (`sid`, `cid`);
+CREATE UNIQUE INDEX `teacher_course_index_10` ON `teacher_course` (`tid`, `cid`);
 
-CREATE INDEX `student_course_index_13` ON `student_course` (`cid`, `sid`);
+CREATE UNIQUE INDEX `teacher_course_index_11` ON `teacher_course` (`cid`, `tid`);
 
-ALTER TABLE `user_role` ADD FOREIGN KEY (`uid`) REFERENCES `user` (`id`);
+CREATE UNIQUE INDEX `student_course_index_12` ON `student_course` (`sid`, `cid`);
 
-ALTER TABLE `user_role` ADD FOREIGN KEY (`rid`) REFERENCES `role` (`id`);
+CREATE UNIQUE INDEX `student_course_index_13` ON `student_course` (`cid`, `sid`);
 
-ALTER TABLE `student_result` ADD FOREIGN KEY (`sid`) REFERENCES `user` (`id`);
+CREATE UNIQUE INDEX `question_course_index_14` ON `question_course` (`qid`, `cid`);
 
-ALTER TABLE `student_result` ADD FOREIGN KEY (`rid`) REFERENCES `result` (`id`);
+CREATE UNIQUE INDEX `question_course_index_15` ON `question_course` (`cid`, `qid`);
 
-ALTER TABLE `questio_type` ADD FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+CREATE UNIQUE INDEX `result_course_index_16` ON `result_course` (`rid`, `cid`);
 
-ALTER TABLE `questio_type` ADD FOREIGN KEY (`qtid`) REFERENCES `questionType` (`id`);
+CREATE UNIQUE INDEX `result_course_index_17` ON `result_course` (`cid`, `rid`);
 
-ALTER TABLE `teacher_question` ADD FOREIGN KEY (`tid`) REFERENCES `user` (`id`);
+ALTER TABLE `user_role`
+ADD
+    FOREIGN KEY (`uid`) REFERENCES `user` (`id`);
 
-ALTER TABLE `teacher_question` ADD FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+ALTER TABLE `user_role`
+ADD
+    FOREIGN KEY (`rid`) REFERENCES `role` (`id`);
 
-ALTER TABLE `question_result` ADD FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+ALTER TABLE `student_result`
+ADD
+    FOREIGN KEY (`sid`) REFERENCES `user` (`id`);
 
-ALTER TABLE `question_result` ADD FOREIGN KEY (`rid`) REFERENCES `result` (`id`);
+ALTER TABLE `student_result`
+ADD
+    FOREIGN KEY (`rid`) REFERENCES `result` (`id`);
 
-ALTER TABLE `teacher_course` ADD FOREIGN KEY (`tid`) REFERENCES `user` (`id`);
+ALTER TABLE `question_type`
+ADD
+    FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
 
-ALTER TABLE `teacher_course` ADD FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
+ALTER TABLE `question_type`
+ADD
+    FOREIGN KEY (`qtid`) REFERENCES `questionType` (`id`);
 
-ALTER TABLE `student_course` ADD FOREIGN KEY (`sid`) REFERENCES `user` (`id`);
+ALTER TABLE `teacher_question`
+ADD
+    FOREIGN KEY (`tid`) REFERENCES `user` (`id`);
 
-ALTER TABLE `student_course` ADD FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
+ALTER TABLE `teacher_question`
+ADD
+    FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+
+ALTER TABLE `question_result`
+ADD
+    FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+
+ALTER TABLE `question_result`
+ADD
+    FOREIGN KEY (`rid`) REFERENCES `result` (`id`);
+
+ALTER TABLE `teacher_course`
+ADD
+    FOREIGN KEY (`tid`) REFERENCES `user` (`id`);
+
+ALTER TABLE `teacher_course`
+ADD
+    FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
+
+ALTER TABLE `student_course`
+ADD
+    FOREIGN KEY (`sid`) REFERENCES `user` (`id`);
+
+ALTER TABLE `student_course`
+ADD
+    FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
+
+ALTER TABLE `question_course`
+ADD
+    FOREIGN KEY (`qid`) REFERENCES `question` (`id`);
+
+ALTER TABLE `question_course`
+ADD
+    FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
+
+ALTER TABLE `result_course`
+ADD
+    FOREIGN KEY (`rid`) REFERENCES `result` (`id`);
+
+ALTER TABLE `result_course`
+ADD
+    FOREIGN KEY (`cid`) REFERENCES `course` (`id`);
