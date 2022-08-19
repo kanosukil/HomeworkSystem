5b0fca78d972195b74acfc2257108574d9da4908
kanosamasi <lhb.88@foxmail.com>
2022-08-19 16:15:45 +0800

    HS 修改 ES 配置, 保证在 docker 网络下能够和 ElasticSearch 容器链接

--- Homework00_ElasticSearch/src/main/java/cn/summer/homework/config/ESClientConfig.java
+++ Homework00_ElasticSearch/src/main/java/cn/summer/homework/config/ESClientConfig.java
@@ -4,8 +4,6 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
@@ -22,26 +20,37 @@ import org.springframework.context.annotation.Configuration;
 public class ESClientConfig {
     private static final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 
-    @Value("${spring.elasticsearch.username}")
-    private String username;
-    @Value("${spring.elasticsearch.password}")
-    private String password;
+    //    @Value("${spring.elasticsearch.username}")
+//    private String username;
+//    @Value("${spring.elasticsearch.password}")
+//    private String password;
     @Value("${spring.elasticsearch.host}")
     private String host;
-    @Value("${spring.elasticsearch.port}")
-    private String port;
+//    @Value("${spring.elasticsearch.port}")
+//    private Integer port;
+
+//    @Value("${spring.elasticsearch.auth.enabled}")
+//    private Boolean isAuth;
 
     // RestClient
     @Bean
     public RestClient restClient() {
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password));
-        return RestClient.builder(
-                        new HttpHost(host, Integer.parseInt(port), "http"))
-                .setHttpClientConfigCallback(httpAsyncClientBuilder
-                        -> httpAsyncClientBuilder
-                        .setDefaultCredentialsProvider(credentialsProvider))
+//        if (isAuth) {
+//            logger.info("ElasticSearch RestClient 用户验证获取");
+//            credentialsProvider.setCredentials(AuthScope.ANY,
+//                    new UsernamePasswordCredentials(username, password));
+//            return RestClient.builder(
+//                            new HttpHost(host, 9200, "http"))
+//                    .setHttpClientConfigCallback(httpAsyncClientBuilder
+//                            -> httpAsyncClientBuilder
+//                            .setDefaultCredentialsProvider(credentialsProvider))
+//                    .build();
+//        } else {
+//        logger.info("ElasticSearch RestClient 无用户验证获取");
+        return RestClient.builder(new HttpHost(host,
+                        9200, "http"))
                 .build();
+//        }
     }
 
     // 阻塞型
